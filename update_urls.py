986a08d22c7e6e37753cb7baa87fdd8856a62828
nutterthanos <minecraftitsover90@gmail.com>
2024-02-26 13:03:24 +1030

    Update update_urls.py

--- update_urls.py
+++ update_urls.py
@@ -1,27 +1,25 @@
 import os
 
-# Function to update start_file_number and end_file_number in vs_valid.py
-def update_vs_valid(start_number_increment, end_number_increment):
+# Function to update the for loop range in vs_valid.py
+def update_vs_valid():
     vs_valid_file = "vs_valid.py"
     
     # Read the contents of vs_valid.py
     with open(vs_valid_file, "r") as f:
         lines = f.readlines()
     
-    # Find and update the lines containing start_file_number and end_file_number
+    # Find and update the line containing the for loop
     new_lines = []
     for line in lines:
-        if line.startswith("start_file_number"):
-            start_number = int(line.split("=")[-1].strip())
-            new_start_number = start_number + start_number_increment
-            new_line = line.replace(str(start_number), str(new_start_number))
-            new_lines.append(new_line)
-        elif line.startswith("end_file_number"):
-            end_number = int(line.split("=")[-1].strip())
-            new_end_number = end_number + end_number_increment
-            new_line = line.replace(str(end_number), str(new_end_number))
+        if "for i in range(1, 5):" in line:
+            new_line = line.replace("for i in range(1, 5):", "for i in range(6, 10):")  # Update range from 1 to 5 to 6 to 10
             new_lines.append(new_line)
         else:
+            # Increment each number in the for loop range by 5
+            for i in range(1, 6):
+                if str(i) in line:
+                    updated_number = str(int(i) + 5)
+                    line = line.replace(str(i), updated_number)
             new_lines.append(line)
     
     # Write the updated lines back to vs_valid.py
@@ -29,5 +27,5 @@ def update_vs_valid(start_number_increment, end_number_increment):
         f.writelines(new_lines)
 
 if __name__ == "__main__":
-    # Increment start_file_number by 5 and end_file_number by 5
-    update_vs_valid(start_number_increment=5, end_number_increment=5)
+    # Update the for loop range
+    update_vs_valid()
