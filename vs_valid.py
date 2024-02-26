6913fd539ae3ceeb1ce5aa106d7c259785bc7dd0
nutterthanos <minecraftitsover90@gmail.com>
2024-02-26 16:12:35 +1030

    Update vs_valid.py

--- vs_valid.py
+++ vs_valid.py
@@ -47,7 +47,7 @@ async def crawl(urls_file: str) -> None:
     await write_valid_urls(valid_urls)
 
 async def main() -> None:
-for i in range(11, 15):        urls_file = f"{URLS_FILE_PREFIX}{i}.txt"
+    for i in range(11, 15):        urls_file = f"{URLS_FILE_PREFIX}{i}.txt"
         logger.info(f"Starting crawl for {urls_file}")
         await crawl(urls_file)
         logger.info(f"Finished crawl for {urls_file}")
